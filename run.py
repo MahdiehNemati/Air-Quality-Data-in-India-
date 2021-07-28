30a06782422e1ff1682d5483f9616384c374fc49
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-07-28 23:38:06 +0930

    changed run.py

--- run.py
+++ run.py
@@ -1,12 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Sum of pollution
-import plotly.express as px
-import pandas
+plt.style.use('seaborn-whitegrid')
 
-from run import city_day
-
-df = city_day.drop(columns = ['Date', 'AQI_Bucket', 'AQI']).groupby('City').sum().reset_index()
-fig = px.treemap(pandas.melt(df, id_vars = 'City'), path=['City','variable'],
-                 values=pandas.melt(df, id_vars = 'City')['value'],
-                 title = 'Cities and the proportion of pollution in each')
-fig.show()
+city_day = pd.read_csv('kaggle/input/air-quality-data-in-india/city_day.csv').sort_values(by=['Date', 'City'])
